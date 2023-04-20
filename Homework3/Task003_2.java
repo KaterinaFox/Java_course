3723f4da9641c7e83a11f4590dbcc31fb3d57f3d
Ekaterina Lisichkina <Katyly@yandex.ru>
2023-04-20 18:22:43 +0300

    homework3

---
+++ Homework3/Task003_2.java
@@ -0,0 +1,15 @@
+﻿//Задание 2. Пусть дан произвольный список целых чисел, удалить из него чётные числа
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task003_2 {
+    public static void main(String[] args) {
+        int[] array = new int[] {1, 4, 0, 5, 3};
+        List<Integer> newArray = new ArrayList<Integer>(array.length);
+        for (int i : array)
+            if (i %2 == 0)
+                newArray.add(i);
+        System.out.println(newArray.toString());
+    }
+}
---
+++ Homework3/Task003_3.java
@@ -0,0 +1,25 @@
+﻿//Задача 3. Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее ариф. из этого списка.
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Task003_3 {
+    public static void main(String[] args) {
+        List<Integer> array = new ArrayList<Integer>(Arrays.asList(1, 4, 0, 5, 3));
+        int minValue = Integer.MAX_VALUE;
+        int maxValue = -1;
+        int sum = 0;
+        for (int i : array) {
+            if (i < minValue)
+                minValue = i;
+            if (i > maxValue)
+                maxValue = i;
+            sum += i;
+        }
+        System.out.println("min value:"+String.valueOf(minValue));
+        System.out.println("max value:"+String.valueOf(maxValue));
+        System.out.println("avg value:"+String.valueOf(1.0*sum/array.size()));
+    }
+
+}
